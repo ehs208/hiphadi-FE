25ca952127973078615573c88519c02bde7b46cb
EunHyunsu <ehs208@naver.com>
2024-11-03 22:26:31 +0900

    fix: 이미지 로드 오류 해결

--- craco.config.js
+++ craco.config.js
@@ -6,7 +6,7 @@ module.exports = {
       '@src': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
       '@pages': path.resolve(__dirname, './src/pages'),
-      '@images': path.resolve(__dirname, './src/public/images'),
+      '@images': path.resolve(__dirname, './src/assets/images'),
       '@api': path.resolve(__dirname, './src/api'),
     },
   },
