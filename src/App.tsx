3ba9da848b4a4ae324e531d8b3fcf81eeba48d6e
EunHyunsu <ehs208@naver.com>
2024-12-15 02:42:34 +0900

    fix: manifest 분리 로직 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 import { useEffect } from 'react';
-import { useLocation } from 'react-router';
+import {
+  useLocation,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import Header from '@components/user/Header';
 import MenuPage from '@pages/user/MenuPage';
 import Footer from '@components/user/Footer';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Cart from '@components/user/Cart';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PrivacyPolicy from '@pages/user/PrivacyPolicy';
 import AdminDashboard from '@pages/admin/AdminDashboard';
 import AdminLogin from '@pages/admin/AdminLogin';
@@ -15,22 +19,29 @@ import ProtectedRoute from '@components/ProtectedRoute';
 
 const queryClient = new QueryClient();
 
-const App: React.FC = () => {
-  const location = useLocation(); // 현재 경로 추적
+const ManifestController: React.FC = () => {
+  const location = useLocation();
 
   useEffect(() => {
     const manifestLink = document.querySelector('link[rel="manifest"]');
-
-    if (location.pathname.startsWith('/admin')) {
-      manifestLink?.setAttribute('href', '%PUBLIC_URL%/admin/manifest.json');
-    } else {
-      manifestLink?.setAttribute('href', '%PUBLIC_URL%/manifest.json');
+    if (manifestLink) {
+      if (location.pathname.startsWith('/admin')) {
+        manifestLink.setAttribute('href', '/admin/manifest.json');
+      } else {
+        manifestLink.setAttribute('href', '/manifest.json');
+      }
     }
   }, [location.pathname]);
+
+  return null;
+};
+
+const App: React.FC = () => {
   return (
-    <div>
-      <Router>
-        <QueryClientProvider client={queryClient}>
+    <Router>
+      <QueryClientProvider client={queryClient}>
+        <ManifestController />
+        <div>
           <Routes>
             <Route
               path="/"
@@ -49,9 +60,9 @@ const App: React.FC = () => {
             </Route>
             <Route path="/admin/login" element={<AdminLogin />} />
           </Routes>
-        </QueryClientProvider>
-      </Router>
-    </div>
+        </div>
+      </QueryClientProvider>
+    </Router>
   );
 };
 
