b4f795a45f08105dc0176bee8ea8c25f1091853a
EunHyunsu <ehs208@naver.com>
2024-12-14 19:29:17 +0900

    feat: api 요청시 쿠키 정보 포함

--- src/lib/axiosConfig.ts
+++ src/lib/axiosConfig.ts
@@ -4,6 +4,7 @@ axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 axios.defaults.withCredentials = true;
 
 export const InstanceGuest = axios.create({
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
