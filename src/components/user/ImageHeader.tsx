b999e2649b46c90c361e6f4ccc6c28eec7f2a84e
EunHyunsu <ehs208@naver.com>
2025-07-14 20:22:41 +0900

    style: 이미지 추가

--- src/components/user/ImageHeader.tsx
+++ src/components/user/ImageHeader.tsx
@@ -1,6 +1,7 @@
 import Slider from 'react-slick';
 import picture1 from '@images/hiphadi1.jpeg';
 import picture2 from '@images/hiphadi2.jpeg';
+import hiphadiEvent from '@images/hiphadi_event.png';
 
 export default function ImageHeader() {
   const settings = {
@@ -14,22 +15,30 @@ export default function ImageHeader() {
 
   return (
     <div className="mb-5">
-      <Slider {...settings}>
-        <div className="w-screen">
+      
+      {/* <Slider {...settings}> */}
+      <div className="w-screen">
+          <img
+            src={hiphadiEvent}
+            alt="힙하디 이벤트"
+            className="w-full object-cover"
+          />
+        </div>
+        {/* <div className="w-screen">
           <img
             src={picture1}
             alt="힙하디 사진1"
             className="w-full object-cover"
           />
-        </div>
-        <div className="w-screen">
+        </div> */}
+        {/* <div className="w-screen">
           <img
             src={picture2}
             alt="힙하디 사진2"
             className="w-full object-cover"
           />
-        </div>
-      </Slider>
+        </div> */}
+      {/* </Slider> */}
     </div>
   );
 }
